df8f729545c67d85e2dffb2c2f673b9c7d6e179d
fjod <fourjustoneday@yandex.ru>
2020-10-22 10:56:16 +0300

    func to find neighbour cells

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -7,33 +7,27 @@ open Shared.GameOfLifeTypes
 open Shared
 open Shared.Utils
 
-type Model =
-    { Grid: CellGrid
-      }
+type Model = { Grid: CellGrid }
 
-type Msg =
-        | Tick of DateTime
+type Msg = Tick of DateTime
 
 let todosApi =
-    Remoting.createApi()
+    Remoting.createApi ()
     |> Remoting.withRouteBuilder Route.builder
     |> Remoting.buildProxy<ITodosApi>
 
-let init(): Model*Cmd<Msg>  =
-    let model =GetRandomCellGrid 50
+let init (): Model * Cmd<Msg> =
+    let model = GetRandomCellGrid 50
     let grid = ToCellGrid model 50
-    let model=  {
-        Grid = grid
-    }
-    (model,??) //pls help here
+    let model = { Grid = grid }
+    (model, Cmd.ofMsg (Tick DateTime.Now))
 
 
 
 
 let update (msg: Msg) (model: Model): Model * Cmd<Msg> =
     match msg with
-    | Tick todos ->
-        { model with Todos = todos }, Cmd.none
+    | Tick time -> { model with Todos = todos }, Cmd.none //here we recalculate state of cells
 
 
 
@@ -42,24 +36,18 @@ open Fable.React.Props
 open Fulma
 
 let navBrand =
-    Navbar.Brand.div [ ]
-        [
-            img [
-                Src "/favicon.png"
-                Alt "FSSF"
-            ]
-            h3 [] [str "Game of Life FSSF"]
-        ]
+    Navbar.Brand.div [] [
+        img [ Src "/favicon.png"; Alt "FSSF" ]
+        h3 [] [ str "Game of Life FSSF" ]
+    ]
 
-let view (model : Model) (dispatch : Msg -> unit) =
-    Hero.hero [
-        Hero.Color IsPrimary
-        Hero.IsFullHeight
-    ] [
-        Hero.head [ ] [
-            Navbar.navbar [ ] [
-                Container.container [ ] [ navBrand ]
+let view (model: Model) (dispatch: Msg -> unit) =
+    Hero.hero [ Hero.Color IsPrimary
+                Hero.IsFullHeight ] [
+        Hero.head [] [
+            Navbar.navbar [] [
+                Container.container [] [ navBrand ]
             ]
         ]
-        Hero.body [][]
+        Hero.body [] []
     ]
--- src/Shared/Utils.fs
+++ src/Shared/Utils.fs
@@ -23,4 +23,26 @@ let ToCellGrid cells size : CellGrid =
         Size = size
     }
 
+let cellFinder _x _y (cell : Cell) =
+            System.Math.Abs((snd cell).X-  _x) < 1 && System.Math.Abs((snd cell).Y-  _y) < 1
+
+let GetNeighbourCells (cell:Cell) (cells: Cell list) : Option<Cell> list =
+
+    //find cell around selected cells one by one; this method returns one cell next to the selected
+    let findNeighbourCell x y  : Option<Cell> =
+        let ret = List.find (cellFinder x y) cells
+        let amount = List.length cells / 2
+        match x, y with
+            | (var1, var2) when var1<=0 || var2 <= 0 -> None
+            | (var1, var2) when var1>= amount || var2 >= amount -> None
+            | _, _ -> Some ret
+
+    let Row = [(snd cell).X-1..(snd cell).X+1]
+    let Col = [(snd cell).Y-1..(snd cell).Y+1]
+    seq {
+        for i in Row do
+            for j in Col do
+                findNeighbourCell i j
+    } |> List.ofSeq
+
 
