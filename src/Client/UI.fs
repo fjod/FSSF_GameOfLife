6ab5194224268c4d971e38733d3afa725f82fbca
fjod <fourjustoneday@yandex.ru>
2020-11-02 14:16:29 +0300

    better button positioning with columns

--- src/Client/UI.fs
+++ src/Client/UI.fs
@@ -28,8 +28,7 @@ let update (msg: Msg) (model: Model): Model =
     match msg with
     | Tick ->
         { model with
-              Grid = CalculateTick model.Grid
-        }
+              Grid = CalculateTick model.Grid }
 
 open Fable.React
 open Fable.React.Props
@@ -45,27 +44,31 @@ let IsEqual (x: int) (y: int) = Math.Abs(x - y) = 0
 
 let renderView (cells: Cell list) x y =
     let cell =
-        List.find (fun (c: Cell) ->  (snd c).X = x &&  (snd c).Y = y) cells
-        //List.find (fun (c: Cell) -> IsEqual (snd c).X x && IsEqual (snd c).Y y) cells
+        List.find (fun (c: Cell) -> (snd c).X = x && (snd c).Y = y) cells
+    //List.find (fun (c: Cell) -> IsEqual (snd c).X x && IsEqual (snd c).Y y) cells
 
     match fst cell with
-    | Dead -> td [ Style [ Background "white"
-                           FontSize 50 ]
-                           ] [ str "***"]
-    | Alive  -> td [ Style [ Background "black"
-                             FontSize 50 ] ]
-                    [str " * "]
-    | DeadOnNextTick -> td [ Style [ Background "white"
-                                     FontSize 50 ] ] [str " * "]
+    | Dead ->
+        td [ Style [ Background "white"
+                     FontSize 50 ] ] [
+            str "***"
+        ]
+    | Alive ->
+        td [ Style [ Background "black"
+                     FontSize 50 ] ] [
+            str " * "
+        ]
+    | DeadOnNextTick ->
+        td [ Style [ Background "white"
+                     FontSize 50 ] ] [
+            str " * "
+        ]
 
 let generateGrid (model: Model) =
-    let listOfCols = [ 0 .. model.Grid.Size-1 ]
-    let listOfCols2 = [ 0 .. model.Grid.Size-1 ]
+    let listOfCols = [ 0 .. model.Grid.Size - 1 ]
+    let listOfCols2 = [ 0 .. model.Grid.Size - 1 ]
 
-    table [ Style [Height 100.
-
-
-                         ] ] [
+    table [ Style [] ] [
         thead [] [
             tr [] [
                 yield th [] []
@@ -90,7 +93,19 @@ let view (model: Model) (dispatch: Msg -> unit) =
             ]
         ]
         Hero.body [] [
-                  Container.container [] [ generateGrid model ]
-            //button [OnClick (fun _ -> dispatch Tick)] [str "Click!"]
+                Columns.columns [] [
+                Column.column [  ] [
+
+                    generateGrid model
+                ]
+                Column.column [ Column.Width(Screen.All, Column.Is2) ] [
+
+                    Button.button [ Button.Size IsLarge
+                                    Button.Color IsInfo
+                                    Button.OnClick(fun _ -> dispatch Tick) ] [
+                        str "Dispatch next Tick"
+                    ]
+                ]
             ]
+        ]
     ]
